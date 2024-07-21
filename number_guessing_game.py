aaedf28dc2441000a68300e53d56d69cfe0696c5
Atemndobs <atemndobs@yahoo.com>
2024-07-21 18:18:40 +0200

    added atem's version of the solution

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -13,3 +13,26 @@
 ##      option2: user's guess is lower than chosen number =>
 #           =>hint=higher,
 #           =>ask user to enter a new number
+
+
+import random
+
+def number_guessing_game():
+    random_number = random.randint(1, 10)
+    guess = None
+    
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 10.")
+    
+    while guess != random_number:
+        guess = int(input("Take a guess: "))
+        
+        if guess < random_number:
+            print("Higher!")
+        elif guess > random_number:
+            print("Lower!")
+        else:
+            print("Congratulations! You've guessed the right number.")
+
+if __name__ == "__main__":
+    number_guessing_game()
