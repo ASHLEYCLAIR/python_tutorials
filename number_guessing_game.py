5aaa47e45016e4e961acc16da47ee96782537fee
ASHLEYCLAIR <njahashleyclaire010@gmail.com>
2024-07-21 17:03:19 +0200

    adding reasoning instructsions

---
+++ number_guessing_game.py
@@ -0,0 +1,15 @@
+# 1. number range 1-10 
+# 2. set our number = 4
+# while loop(to keep asking guesses)
+# 3.ask the user to guess
+# 4. check if the user's input equal to chosen number
+# 4a. if user's guess is correct
+#       => print congratulation 
+#       =>end
+# 4b. if user guess is wrong 
+##      option1: user's guess is higher than chosen number=> 
+#           =>hint=lower, 
+#           =>ask user to enter a new number
+##      option2: user's guess is lower than chosen number =>
+#           =>hint=higher,
+#           =>ask user to enter a new number
